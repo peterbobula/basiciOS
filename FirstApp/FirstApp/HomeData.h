03f3c7228ec99a92cac02e6181e01fc865a6777d
Peter Bobula <peter.bobula@skyscanner.net>
2015-12-03 13:10:08 +0100

    Add homedata files

---
+++ FirstApp/FirstApp/HomeData.h
@@ -0,0 +1,13 @@
+//
+//  HomeData.h
+//  FirstApp
+//
+//  Created by Peter Bobula on 03/12/15.
+//  Copyright © 2015 Peter Bobula. All rights reserved.
+//
+
+#ifndef HomeData_h
+#define HomeData_h
+
+
+#endif /* HomeData_h */
---
+++ FirstApp/FirstApp/HomeData.m
@@ -0,0 +1,9 @@
+//
+//  HomeData.m
+//  FirstApp
+//
+//  Created by Peter Bobula on 03/12/15.
+//  Copyright © 2015 Peter Bobula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
